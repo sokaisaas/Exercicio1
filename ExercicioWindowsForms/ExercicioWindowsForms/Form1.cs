9d91f841e23e67363f97c21a3893de879ad08682
Elena Melink <melinkelena9@gmail.com>
2025-06-02 09:23:39 -0300

    Exercicio1

---
+++ ExercicioWindowsForms/ExercicioWindowsForms/Form1.cs
@@ -0,0 +1,20 @@
+namespace ExercicioWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Olá, Windows Forms!";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
